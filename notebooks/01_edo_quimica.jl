a7d11a3239ed763924f3f0e46be17b6fa731a354
Juan-Huertas <juanestebanhu@gmail.com>
2026-04-21 07:37:41 -0500

    Creacion de los notebooks

---
+++ notebooks/01_edo_quimica.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.20.24
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 53a63090-3d7e-11f1-9b41-e576a62a3098
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.6"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═53a63090-3d7e-11f1-9b41-e576a62a3098
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
